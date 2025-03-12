019e5bebd7182da3c30203c844ba6df0bd8e8d83
Lim0_C <dladudcks22@naver.com>
2025-03-12 14:49:59 +0900

    DO-8 게임화면 약식 UI 및 스톤 객체, Enums 생성

---
+++ Assets/Script/Game/Enums.cs
@@ -0,0 +1,10 @@
+public class Enums
+{
+    public enum GameResult{None, Win, Lose, Draw}
+    
+    public enum PlayerTpe{None, PlayerA, PlayerB}
+    
+    public enum StoneType{None, White, Black}
+    
+    public enum StoneState{ None, Selected, Blocked, Positioned}
+}
