5b60f2d1482c0e9b4f8a7f04c1a64c4a3eb2ce55
Lim0_C <dladudcks22@naver.com>
2025-03-12 16:11:01 +0900

    수정
    미숙한 깃 사용

---
+++ Assets/Script/Game/Stone.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer stoneMarkerSpriteRenderer;
+    [SerializeField] private Sprite[] stoneSprites;
+
+    private int index;
+    private SpriteRenderer _spriteRenderer;
+    
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    
+}
