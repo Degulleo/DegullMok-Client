81b7a6a033e69b8b08dab50419feb91ec4e47e26
Jay <96156114+jaydev00a@users.noreply.github.com>
2025-03-12 17:39:26 +0900

    [UPDATE] 회원가입 및 로그인 구현

---
+++ Assets/Script/Game/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : Singleton<GameManager>
+{
+    [SerializeField] private GameObject signinPanel;
+    [SerializeField] private GameObject signupPanel;
+    
+    private Canvas _canvas;
+    
+    private void Start()
+    {
+        // 로그인
+        OpenSigninPanel();
+    }
+    
+    public void OpenSigninPanel()
+    {
+        if (_canvas != null)
+        {
+            var signinPanelObject = Instantiate(signinPanel, _canvas.transform);
+        }
+    }
+
+    public void OpenSignupPanel()
+    {
+        if (_canvas != null)
+        {
+            var signupPanelObject = Instantiate(signupPanel, _canvas.transform);
+        }
+    }
+
+    protected override void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        _canvas = GameObject.FindObjectOfType<Canvas>();
+    }
+}
