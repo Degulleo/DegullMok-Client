ed9e8664a807f7508abbe143212868eec4af4690
fiore <cjsdlf44@gmail.com>
2025-03-19 17:10:53 +0900

    DO-31 Refactor: 중복되는 클래스 통합
    - RenjuRuleChecker.cs 삭제
    - RenjuForbiddenMoveDetector.cs로 통합

--- Assets/Script/Renju/RenjuForbiddenMoveDetector.cs
+++ Assets/Script/Renju/RenjuForbiddenMoveDetector.cs
@@ -2,10 +2,12 @@
 using UnityEngine;
 
 
-public class RenjuForbiddenMoveDetector
+public class RenjuForbiddenMoveDetector : ForbiddenDetectorBase
 {
     // 렌주 룰 금수 감지기 생성
-    private RenjuRuleChecker _ruleChecker = new RenjuRuleChecker();
+    private RenjuOverlineDetector _overlineDetactor = new();
+    private RenjuDoubleFourDetector _doubleFourDetactor = new();
+    private RenjuDoubleThreeDetector _doubleThreeDetector = new();
 
     /// <summary>
     /// 렌주 룰로 금수 리스트를 반환하는 함수
@@ -15,6 +17,40 @@ public class RenjuForbiddenMoveDetector
     public List<Vector2Int> RenjuForbiddenMove(Enums.PlayerType[,] board)
     {
         var tempBoard = (Enums.PlayerType[,])board.Clone();
-        return _ruleChecker.GetForbiddenMoves(tempBoard);
+
+        List<Vector2Int> forbiddenMoves = new();
+        for (int row = 0; row < BoardSize; row++)
+        {
+            for (int col = 0; col < BoardSize; col++)
+            {
+                // ** 비어 있지 않으면 검사할 필요 없음 **
+                if (!IsEmptyPosition(tempBoard, row, col)) continue;
+
+                // 장목 검사
+                if (_overlineDetactor.IsOverline(tempBoard, row, col))
+                {
+                    Debug.Log("장목 금수 좌표 X축 : " + row + ", Y축 : " + col);
+                    forbiddenMoves.Add(new Vector2Int(row, col));
+                    continue;
+                }
+
+                // 4-4 검사
+                if (_doubleFourDetactor.IsDoubleFour(tempBoard, row, col))
+                {
+                    Debug.Log("사사 금수 좌표 X축 : " + row + ", Y축 : " + col);
+                    forbiddenMoves.Add(new Vector2Int(row, col));
+                    continue;
+                }
+
+                // 3-3 검사
+                if (_doubleThreeDetector.IsDoubleThree(tempBoard, row, col))
+                {
+                    Debug.Log("삼삼 금수 좌표 X축 : " + row + ", Y축 : " + col);
+                    forbiddenMoves.Add(new Vector2Int(row, col));
+                }
+            }
+        }
+
+        return forbiddenMoves;
     }
 }
