e81708c22c69d6b6379fb7d15494f54a2f4565bf
HaeinLEE <heyin0122@korea.ac.kr~>
2025-03-25 17:14:55 +0900

    [Feat] RatingPanel에서 이전 rating과 score 가져와서 반영

--- Assets/Script/UI/PanelChildController/RatingPointsController.cs
+++ Assets/Script/UI/PanelChildController/RatingPointsController.cs
@@ -5,11 +5,32 @@ using UnityEngine.UI;
 
 public class RatingPointsController : MonoBehaviour
 {
-    [SerializeField] Image[] minusImages;
-    [SerializeField] Image[] plusImage;
+    [SerializeField] GameObject[] minusImages;
+    [SerializeField] GameObject[] plusImage;
+    [SerializeField] private float flipDuration = 0.3f;
     
     private Color32 _minusColor = new Color32(255, 0, 0, 255);
     private Color32 _plusColor = new Color32(34, 87, 255, 255);
     private Color32 _defaultColor = new Color32(176, 176, 176, 255);
-    
+
+    public void InitRatingPoints(int oldScore)
+    {
+        if (oldScore == 0)
+        {
+            return;
+        }else if (oldScore > 0)
+        {
+            for (int i = 0; i < oldScore; i++)
+            {
+                plusImage[i].GetComponent<Image>().color = _plusColor;
+            }
+        }
+        else if (oldScore < 0)
+        {
+            for (int i = oldScore; i < 0; i++)
+            {
+                minusImages[minusImages.Length+i].GetComponent<Image>().color = _minusColor;
+            }
+        }
+    }
 }
